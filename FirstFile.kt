fc5d9be686a3709a332b532ccd870cb985d64c83
Manjit Pal <155907446+palManjit@users.noreply.github.com>
2024-03-30 11:54:34 +0530

    Add files via upload

---
+++ FirstFile.kt
@@ -0,0 +1,16 @@
+package com.jp.firstapp.FirstProg
+
+fun main(){
+    val t=Add()
+    var b=Add()
+
+    println(b.a)
+    println(b.t)
+    print(b.c)
+}
+class Add{
+    var a=10
+    var t:String="Manjit Pal"
+    var c:Float=45.89f
+
+}
